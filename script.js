9d1362cbc6a8f6ca3388318b6adaa6e8d5df3376
Meydson Baracho <64757536+meydson@users.noreply.github.com>
2024-08-19 20:34:42 -0300

    teste botão

--- script.js
+++ script.js
@@ -64,3 +64,19 @@ function saldo() {
 function limite() {
     console.log(conta1.verificarLimite());
 }
+
+function teste() {
+    var meuObj = new Object(),
+    str = "minhaString",
+     aleat = Math.random(),
+    obj = new Object();
+
+    meuObj.tipo = "Sintaxe de ponto";
+    meuObj["data de criacao"] = "String com espaco";
+    meuObj[str] = "valor de String";
+    meuObj[aleat] = "Numero Aleatorio";
+    meuObj[obj] = "Objeto";
+    meuObj[""] = "Mesmo uma string vazia";
+
+    console.log(meuObj);
+}
