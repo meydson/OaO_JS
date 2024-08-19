7a895f38a0698bc98c55f195cae6222784891f42
Meydson Baracho <64757536+meydson@users.noreply.github.com>
2024-08-19 20:03:14 -0300

    inserido os métodos do objeto

--- script.js
+++ script.js
@@ -4,15 +4,63 @@ class ContaBanco {
         this.dtnasc = "dtnasc";
         this.tel = "tel";
         this.saldo = 0;
+        this.limite = 0;
     }
+    verificarSaldo() {
+        return this.saldo;
+    }
+    depositar(valor) {
+        this.saldo += valor;
+    }
+    sacar(valor) {
+        this.saldo -= valor;
+    }
+    emprestimo(valor) {
+        this.limite -= valor;
+        this.saldo += valor;
+    }
+    
+    verificarLimite() {
+        return this.limite;
+    }
+
+
 }
 
-function teste() {
-    let conta1 = new ContaBanco();
+let conta1;
+
+function criarConta() {
+    conta1 = new ContaBanco();
     conta1.nome = prompt("nome");
     conta1.dtnasc = prompt("data nascimento");
     conta1.tel = prompt("telefone");
-    conta1.saldo = parseFloat(prompt("saldo"))
-
+    conta1.saldo = parseFloat(prompt("saldo"));
+    conta1.limite = 1000;
     console.log(conta1);
+}
+
+function depositoConta() {
+    let valor = parseFloat(prompt("Informe o valor para depósito"));
+    conta1.depositar(valor);
+    console.log(conta1.saldo);
+}
+
+function sacarConta() {
+    let valor = parseFloat(prompt("Informe o valor para saque"));
+    conta1.sacar(valor);
+    console.log(conta1.saldo)
+}
+
+function emprestimoConta() {
+    let valor = parseFloat(prompt("Informe o valor do empréstimo"));
+    conta1.emprestimo(valor);
+    console.log(conta1.saldo);
+}
+
+function saldo() {
+    console.log(conta1.verificarSaldo());
+}
+
+function limite() {
+    console.log(conta1.verificarLimite());
 }
